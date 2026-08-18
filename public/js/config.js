a674cb597dc9ea19e4f9c5488bf2ddeab2020e4c
pmarcelacademy1 <pmarcelacademy@gmail.com>
2026-08-19 00:32:57 +0100

    well

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://two47expertstrades.onrender.cosm';
+const API_BASE_URL = 'https://two47expertstrades.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
